57775ae7236ae2ffa6ab4fb1527b746cfe98db94
Javier Peralta Saenz <jiperaltas@outlook.com>
2020-01-30 19:46:47 -0600

    Remove unused code

--- pritunl/files/pritunl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# generate ssl cert
-pritunl set app.acme_domain {{ grains.id }}
-pritunl set app.acme_key "openssl genrsa 4096"
-pritunl set app.acme_timestamp `python -c 'import time;print time.time()'`
-pritunl set app.acme_renew 0
